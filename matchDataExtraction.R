a0e7eaf02aaec12f4009489ce97cae88e92e4b80
JackP <noneBusiness@gmail.com>
2018-06-07 23:24:36 +0100

    Added looping logic to extract any number of historical matches.

--- matchDataExtraction.R
+++ matchDataExtraction.R
@@ -3,34 +3,40 @@
 # Note that rate limits are enforced per region. For example, with the above rate limit, you could make 20 requests every 1 seconds to both NA and EUW endpoints simultaneously.
 library(jsonlite)
 library(tidyverse)
+library(lubridate)
 
 # api_key stored in .Rprofile.site
 Region <- "euw1"
 Summoner_Name <- "Proudfeet"
 
 Summoner_Info_Link <- paste0("https://",Region,".api.riotgames.com/lol/summoner/v3/summoners/by-name/",Summoner_Name,"?api_key=",api_key)
-Summoner_Info_data <- fromJSON(testLink, simplifyDataFrame = TRUE)
+Summoner_Info_data <- fromJSON(Summoner_Info_Link, simplifyDataFrame = TRUE)
 
 Summoner_ID <- Summoner_Info_data$accountId
 
 Match_His_Link <- paste("https://",Region,".api.riotgames.com/lol/match/v3/matchlists/by-account/",Summoner_ID,"?api_key=",api_key,sep = "")
 Match_history <- fromJSON(Match_His_Link)
-nGames <- Match_history$totalGames
+# nGames <- Match_history$totalGames
 
-# nLoops <- 1:ceiling((nGames-100)/100)
-# extractMatches <- function(x) {
-#   start <- (x*100)+1
-#   end <- min((start+100)-1,nGames)
-#   linkText <- paste0("https://",Region,".api.riotgames.com/lol/match/v3/matchlists/by-account/",Summoner_ID,"?beginIndex=",start,"&endIndex=",end,"&api_key=",api_key)
-#   mathes <- fromJSON(linkText, flatten = TRUE)
-#   mathes$matches[[1]]
-# }
-# map(nLoops,extractMatches)
 
-matchDF <- Match_history$matches %>% 
-             mutate(date = lubridate::as_datetime(timestamp/1000, origin = lubridate::as_datetime("1970-01-01")))
+# Bulk extract match meta data --------------------------------------------
 
-matchRefs <- pull(matchDF,gameId)
+
+extractMatches <- function(account,start=NULL,end=NULL,REgion = Region,apiKey=api_key){
+  
+paste0("https://",Region,".api.riotgames.com/lol/match/v3/matchlists/by-account/",Summoner_ID,"?beginIndex=",start,"&endIndex=",end,"&api_key=",apiKey)
+}
+nGames <- 300
+matchHistDF<- tibble(nGamesEnd = as.integer(seq(100,nGames,by=100)),
+                     nGamesStart = as.integer(lag(nGamesEnd, default = 0L))) %>% 
+                mutate(link=map2(nGamesStart, nGamesEnd,~extractMatches(account = account,start = .x,end = .y)),
+                       rawJson=map(link,fromJSON),
+                       df = map(rawJson,"matches")) %>% 
+                  unnest(df,drop = TRUE) %>% 
+                    mutate(date = as_datetime(timestamp/1000, origin = as_datetime("1970-01-01")))
+
+
+##################
 
 getMatchStats <- function(x){
   matchLink<- paste0("https://",Region,".api.riotgames.com/lol/match/v3/matches/",x,"?api_key=",api_key)
